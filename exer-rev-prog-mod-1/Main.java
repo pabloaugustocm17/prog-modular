e95ab7f2d337f23b501fca2d0b4d7a374e9087d8
Pablo Magalhães <109962972+pabloaugustocm17@users.noreply.github.com>
2022-08-05 20:28:15 -0300

    ADD exercicio-1
    Exercício 1 completo, junto aos testes

--- exer-rev-prog-mod-1/Main.java
+++ exer-rev-prog-mod-1/Main.java
@@ -1,7 +1,74 @@
+import java.util.Scanner;
+
 public class Main{
 
     public static void main(String[] args) {
-        
+
+        Scanner teclado = new Scanner(System.in);
+
+        Exercicio1(teclado);
+
+
     }
+
+    /* Menu Exercícios */
+
+    public static void Exercicio1(Scanner teclado){
+
+        int[] vetor = CriaVetor(6, teclado);
+        
+        ImprimiVetor(vetor);
+
+        vetor = InverteVetor(vetor);
+
+        ImprimiVetor(vetor);
+
+
+    }
+
+    /* Utilitarios */
+
+    private static void ImprimiVetor(int[] vetor){
+
+        for(int i = 0; i < vetor.length; i++){
+
+            System.out.println(vetor[i]);
+        }
+
+    }
+
+    /* Exercício 1 */
+
+    private static int[] CriaVetor(int tamanho_vetor, Scanner teclado){
+
+        int[] vetor_inteiro = new int[tamanho_vetor];
+
+        for(int i = 0; i < tamanho_vetor; i++){
+
+            vetor_inteiro[i] = teclado.nextInt();
+
+        }
+
+        return vetor_inteiro;
+
+
+    }
+
+    private static int[] InverteVetor(int[] vetor_inteiro){
+
+        int[] vetor_auxiliar = new int[vetor_inteiro.length];
+        int posicao_inversa = vetor_auxiliar.length - 1;
+
+        for(int i = 0; i < vetor_inteiro.length; i++){
+
+            vetor_auxiliar[posicao_inversa] = vetor_inteiro[i];
+            posicao_inversa--;
+
+        }
+
+        return vetor_auxiliar;
+
+    }
+
 
 }
