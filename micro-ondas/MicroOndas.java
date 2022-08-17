205de288109bb8e9cc8748a5e5f7010a4a7a351c
Pablo Magalhães <109962972+pabloaugustocm17@users.noreply.github.com>
2022-08-17 11:31:43 -0300

    ADD MetodoAdicionarTempo
    O método criado na classe ´Tempo´, adiciona o tempo passado pelo usuário.

--- micro-ondas/MicroOndas.java
+++ micro-ondas/MicroOndas.java
@@ -1,8 +1,49 @@
-import java.util.Calendar;
-
 public class MicroOndas {
     
-    
+    private Tempo cronometro;
     private boolean is_fechado;
     
+    private void init(){
+        this.cronometro = new Tempo();
+        this.is_fechado = true;
+    }
+
+    public MicroOndas(){
+        init();
+    }
+
+    public void AdicionaTempo(int tempo_adicionar, String valor_atualizado){
+
+        switch (valor_atualizado) {
+
+            case "Segundos":
+                this.cronometro.AdicionaTempo(tempo_adicionar, "Segundos");
+                break;
+
+            case "Minutos":
+                this.cronometro.AdicionaTempo(tempo_adicionar, "Minutos");
+                break;
+
+            case "Horas":
+                this.cronometro.AdicionaTempo(tempo_adicionar, "Horas");
+                break;
+
+        }
+
+
+    }
+
+    public Tempo getCronometro() {
+        return cronometro;
+    }
+
+    public boolean IsFechado() {
+        return is_fechado;
+    }
+
+    public void setIsFechado(boolean is_fechado) {
+        this.is_fechado = is_fechado;
+    }
+    
 }
+
--- micro-ondas/Tempo.java
+++ micro-ondas/Tempo.java
@@ -1,10 +1,10 @@
-public class Tempo {
-    
+class Tempo {
+
     private int horas;
     private int minutos;
     private int segundos;
 
-    private void init(int horas, int minutos, int segundos){
+    private void init(int horas, int minutos, int segundos) {
 
         this.horas = horas;
         this.minutos = minutos;
@@ -12,35 +12,104 @@ public class Tempo {
 
     }
 
-    public Tempo(){
+    public Tempo() {
         init(0, 0, 0);
     }
 
-    public int getHoras() {
-        return horas;
+    public void AdicionaTempo(int tempo_adicionar, String valor_atualizado) {
+
+        switch (valor_atualizado) {
+
+            case "Segundos":
+                AtualizarTempo(tempo_adicionar, true);
+                break;
+
+            case "Minutos":
+                AtualizarTempo(tempo_adicionar, false);
+                break;
+
+            case "Horas":
+                AtualizarHoras(tempo_adicionar);
+                break;
+
+        }
+
     }
 
-    public void setHoras(int horas) {
-        this.horas = horas;
+    private boolean Verificacao60(int valor, boolean is_segundos) {
+
+        if (valor >= 60 || valor < 0) {
+
+            System.out.print("Valor para ");
+
+            if (is_segundos) {
+                System.out.print("segundos ");
+            } else {
+                System.out.print(" minutos ");
+            }
+
+            System.out.println("informados estão errados");
+
+            return false;
+        } else {
+            return true;
+        }
+
     }
 
-    public int getMinutos() {
-        return minutos;
+    private void AtualizarHoras(int horas) {
+
+        if (horas >= 1) {
+            System.err.println("O micro ondas ou sua paciência irá explodir");
+        }
+
+        this.horas += horas;
+
     }
 
-    public void setMinutos(int minutos) {
-        this.minutos = minutos;
+    private void AtualizarTempo(int tempo_atualizar, boolean is_segundos) {
+
+        if (Verificacao60(tempo_atualizar, is_segundos)) {
+
+            int tempo_menor_adicionar = 0;
+            int tempo_maior_adicionar = 0;
+
+            int tempo_menor_atual = 0;
+            int tempo_maior_atual = 0;
+
+            if (is_segundos) {
+                tempo_menor_atual = this.segundos;
+                tempo_maior_atual = this.minutos;
+            } else {
+                tempo_menor_atual = this.minutos;
+                tempo_maior_atual = this.horas;
+            }
+
+            int soma_tempo_menor = tempo_menor_atual + tempo_atualizar;
+
+            if (soma_tempo_menor >= 60) {
+
+                tempo_maior_adicionar = soma_tempo_menor % 60;
+                tempo_menor_adicionar = soma_tempo_menor - (60 * tempo_maior_adicionar);
+                tempo_maior_adicionar += tempo_maior_atual;
+
+            } else {
+
+                tempo_menor_adicionar = soma_tempo_menor;
+                tempo_maior_adicionar = tempo_maior_atual;
+
+            }
+
+            if (is_segundos) {
+                this.segundos = tempo_menor_adicionar;
+                this.minutos = tempo_maior_adicionar;
+            } else {
+                this.minutos = tempo_menor_adicionar;
+                AtualizarHoras(tempo_maior_adicionar);
+            }
+
+        }
+
     }
-
-    public int getSegundos() {
-        return segundos;
-    }
-
-    public void setSegundos(int segundos) {
-        this.segundos = segundos;
-    }
-
-    
-
 
 }
