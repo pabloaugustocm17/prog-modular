1c333fe02becefd7b762cf17db7efd26682a40ad
Pablo Magalhães <109962972+pabloaugustocm17@users.noreply.github.com>
2022-08-29 09:51:28 -0300

    UPDATE

--- tdd-veiculo/Veiculo.java
+++ tdd-veiculo/Veiculo.java
@@ -1,5 +1,5 @@
 public class Veiculo {
-    
+
     private String placa_veiculo;
     private String data_aquisicao;
     private double consumo_litro;
@@ -8,7 +8,8 @@ public class Veiculo {
 
     /* Construtores */
 
-    private void init(String placa_veiculo, String data_aquisicao, double consumo_litro, double km_total, double combustivel_maximo){
+    private void init(String placa_veiculo, String data_aquisicao, double consumo_litro, double km_total,
+            double combustivel_maximo) {
 
         this.placa_veiculo = placa_veiculo;
         this.data_aquisicao = data_aquisicao;
@@ -18,46 +19,69 @@ public class Veiculo {
 
     }
 
-    public Veiculo(){
+    public Veiculo() {
         init("", "", 0, 0, 0);
     }
-    
-    public Veiculo(String placa_veiculo, double km_total, double combustivel_maximo){
+
+    public Veiculo(String placa_veiculo, double km_total, double combustivel_maximo) {
         init(placa_veiculo, "", 0, km_total, combustivel_maximo);
     }
 
-    public Veiculo(String placa_veiculo, String data_aquisicao ,double consumo_litro, double km_total, double combustivel_maximo){
+    public Veiculo(String placa_veiculo, String data_aquisicao, double consumo_litro, double km_total,
+            double combustivel_maximo) {
         init(placa_veiculo, data_aquisicao, consumo_litro, km_total, combustivel_maximo);
     }
 
     /* A */
 
-    public void RegistraDataAquisicao(){
+    public String RegistraDataAquisicao() {
 
-        Main.ImprimiMensagem("Informe a data de aquisição do veículo: ");
-        String data_aquisicao = Main.teclado.nextLine();
+        Util.ImprimiMensagem("Informe a data de aquisição do veículo: ");
+        String data_aquisicao = Util.teclado.nextLine();
+        boolean data_correta = Util.VerificaData(data_aquisicao);
+
+        while (!data_correta) {
+
+            Util.ImprimiMensagem("Informe novamente a data de aquisição de forma correta: ");
+            data_aquisicao = Util.teclado.nextLine();
+            data_correta = Util.VerificaData(data_aquisicao);
+
+        }
+
         setDataAquisicao(data_aquisicao);
 
+        return data_aquisicao;
 
     }
 
-    private void setDataAquisicao(String data_aquisicao){
-        
+    private void setDataAquisicao(String data_aquisicao) {
+
         this.data_aquisicao = data_aquisicao;
 
     }
 
     /* B */
 
-    public void RegistraConsumoLitro(){
+    public double RegistraConsumoLitro() {
 
-        Main.ImprimiMensagem("Informe o consumo por litro do veículo: ");
-        double consumo_litro = Main.teclado.nextDouble();
-        setConsumoLitro(consumo_litro);
+        Util.ImprimiMensagem("Informe o consumo por litro do veículo: ");
+
+        try {
+
+            double consumo_litro = Util.teclado.nextDouble();
+            setConsumoLitro(consumo_litro);
+            return consumo_litro;
+
+        } catch (Exception e) {
+
+            Util.ImprimiMensagem("ERROR" + e.getMessage());
+            return 0;
+
+        }
 
     }
 
-    private void setConsumoLitro(double consumo_litro){
+    private void setConsumoLitro(double consumo_litro) {
 
         this.consumo_litro = consumo_litro;
 
@@ -65,38 +89,38 @@ public class Veiculo {
 
     /* C */
 
-    public double getKmtotal(){
+    public double getKmtotal() {
         return this.km_total;
-    } 
+    }
 
     /* D */
 
-    public void CalcularKmMedia(){
+    public void CalcularKmMedia() {
 
-        Main.ImprimiMensagem("Informe a quilometragem da rota: ");
-        double rota = Main.teclado.nextDouble();
+        Util.ImprimiMensagem("Informe a quilometragem da rota: ");
+        double rota = Util.teclado.nextDouble();
 
         RetornaMediaRota(rota);
 
-    }  
+    }
 
-    private double RetornaMediaRota(double rota){
+    private double RetornaMediaRota(double rota) {
 
         return Math.floor(rota / consumo_litro);
-        
+
     }
 
     /* E */
-    
-    public void CalcularTanqueCheio(){
 
-        Main.ImprimiMensagem("Informe o preço do combustível: ");
-        double preco_combustivel = Main.teclado.nextDouble();
+    public void CalcularTanqueCheio() {
+
+        Util.ImprimiMensagem("Informe o preço do combustível: ");
+        double preco_combustivel = Util.teclado.nextDouble();
 
         RetornaValorTanqueCheio(preco_combustivel);
     }
 
-    private double RetornaValorTanqueCheio(double preco_combustivel){
+    private double RetornaValorTanqueCheio(double preco_combustivel) {
 
         return Math.floor(this.combustivel_maximo * preco_combustivel);
 
@@ -104,22 +128,22 @@ public class Veiculo {
 
     /* Util */
 
-    public void ImprimiVeiculo(){
-        Main.ImprimiMensagem("Placa veiculo: " + this.placa_veiculo);
-        Main.ImprimiMensagem("Data de aquisição: " + this.data_aquisicao);
-        Main.ImprimiMensagem("Consumo por litro: " + this.consumo_litro);
-        Main.ImprimiMensagem("Quilometragem atual: " + this.km_total);
-        Main.ImprimiMensagem("Cobustível máximo do tanque: " + this.combustivel_maximo);
+    public void ImprimiVeiculo() {
+        Util.ImprimiMensagem("Placa veiculo: " + this.placa_veiculo);
+        Util.ImprimiMensagem("Data de aquisição: " + this.data_aquisicao);
+        Util.ImprimiMensagem("Consumo por litro: " + this.consumo_litro);
+        Util.ImprimiMensagem("Quilometragem atual: " + this.km_total);
+        Util.ImprimiMensagem("Cobustível máximo do tanque: " + this.combustivel_maximo);
     }
 
-    public static Veiculo CriaVeiculo(){
+    public static Veiculo CriaVeiculo() {
 
-        Main.ImprimiMensagem("Informe a placa do veiculo: ");
-        String placa_veiculo = Main.teclado.nextLine();
-        Main.ImprimiMensagem("Informe a quilometragem atual do veiculo: ");
-        double km_total = Main.teclado.nextDouble();
-        Main.ImprimiMensagem("Informe a quantidade de combustível máximo do veículo: ");
-        double combustivel_maximo = Main.teclado.nextDouble();
+        Util.ImprimiMensagem("Informe a placa do veiculo: ");
+        String placa_veiculo = Util.teclado.nextLine();
+        Util.ImprimiMensagem("Informe a quilometragem atual do veiculo: ");
+        double km_total = Util.teclado.nextDouble();
+        Util.ImprimiMensagem("Informe a quantidade de combustível máximo do veículo: ");
+        double combustivel_maximo = Util.teclado.nextDouble();
 
         Veiculo veiculo = new Veiculo(placa_veiculo, km_total, combustivel_maximo);
 
--- tdd-veiculo/VeiculoTest.java
+++ tdd-veiculo/VeiculoTest.java
@@ -3,20 +3,38 @@ import org.junit.Test;
 
 public class VeiculoTest {
 
+    private Veiculo veiculo;
+
     public VeiculoTest(){
-        
+        this.veiculo = new Veiculo("123-7894", 500 , 120);
     }
 
     @Test
-    public void CriarVeiculo(){
+    public void RegistraDataAquisicao(){
         
-        Main.ImprimiMensagem("Cria Carro");
+        String data_aquisicao = this.veiculo.RegistraDataAquisicao();
 
-        Veiculo veiculo = new Veiculo("123456", "123456", 12, 13, 120);
-
-        assertEquals(veiculo, Veiculo.CriaVeiculo());
-        
+        assertEquals("12/03/2000", data_aquisicao);
 
     }
+
+    @Test
+    public void RegistraConsumoLitro(){
+
+        double consumo_litro = this.veiculo.RegistraConsumoLitro();
+
+        assertEquals("20", Double.toString(consumo_litro));
+
+    }
+
+    @Test
+    public void SaberKmTotalVeiculo(){
+
+        assertEquals("500", this.veiculo.getKmtotal());
+
+    }
+
+
+
 
 }
